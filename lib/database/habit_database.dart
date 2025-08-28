ccfe6b3377b061553f1b21dd5b150bee5bb5b397
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-08-28 22:31:33 +0530

    habit_tracker

--- lib/database/habit_database.dart
+++ lib/database/habit_database.dart
@@ -1,13 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:habit_tracker/models/app_settings.dart';
+import 'package:habit_tracker/models/habit.dart';
 import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
 
 class HabitDatabase extends ChangeNotifier {
   static late Isar isar;
 
   // S E T U P
   // Initialize Isar database
+  static Future<void> initilize() async {
+    final dir = await getApplicationDocumentsDirectory();
+    isar = await Isar.open([
+      HabitSchema,
+      AppSettingsSchema,
+    ], directory: dir.path);
+  }
+
   // save first launch date
+  Future<void> saveFirstLaunchDate() async {
+    final existingSettings = await isar.appSettings.where().findFirst();
+    if (existingSettings == null) {
+      final settings = AppSettings()..FirstLaunchDate = DateTime.now();
+      await isar.writeTxn(() => isar.appSettings.put(settings));
+    }
+  }
+
   // get first launch date
+  Future<DateTime?> getFirstLaunchDate() async {
+    final settings = await isar.appSettings.where().findFirst();
+    return settings?.FirstLaunchDate;
+  }
 
   // C U R D O P E R A T I O N S
   // list of habits
@@ -16,4 +39,4 @@ class HabitDatabase extends ChangeNotifier {
   // update habit name
   // delete habit
 }
-//4:17:27
+//4:19:50
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:habit_tracker/database/habit_database.dart';
 import 'package:habit_tracker/pages/home_page.dart';
 import 'package:habit_tracker/theme/light_mode.dart';
 import 'package:habit_tracker/theme/theme_provider.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  // initilize database
+  await HabitDatabase.initilize();
+  await HabitDatabase().saveFirstLaunchDate();
   runApp(
     ChangeNotifierProvider(
       create: (context) => ThemeProvider(),
